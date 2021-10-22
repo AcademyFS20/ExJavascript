dfbe4230281ccaebfe5f883fa3bd57622bfbc207
Marouane <m.marouane14@gmail.com>
2021-10-22 11:26:04 +0100

    Javascript Execises"

--- Dossier Javascript 1/myscript.js
+++ Dossier Javascript 1/myscript.js
@@ -22,4 +22,68 @@ else{
          
 
 
+//WORKSHOP1
+function moneySum (array){
+
+let count = 0
+for (var i = 0; i <= array.length; i++){
+    return count = count + array[i].budget
+    }
+
+}
+
+console.log(moneySum([
+    { name: "amine", age: 21, budget: 15000 },
+    { name: "hamid",  age: 32, budget: 40000 },
+    { name: "soufiane",  age: 16, budget: 3500 }
+    ]))
+
+
+
+
+
+//WORKSHOP2
+function shipping(array){
+
+let totalOrder = array.reduce(function (acc, cur) {
+    return acc + cur.msgCount
+},0);
+
+if (totalOrder > 50){
+    return "True"
+}
+else {    
+    return "False"
+}
+
+}
+
+console.log(shipping([
+    { "tomate": 50.99, "thon": 50.99 }
+]));
+
+
+
+
+
+//WORKSHOP 4:
+function bestNote(array){
+
+if (array.length < 1) {
+    return array.push((array1.name) + Math.max(array.notes))
+}
+
+else {
+    return  array.name + [0]
+}
+}
+console.log(bestNote([
+    { name: "amine", notes: [3, 5, 4]}
+  ]))
+
+
+
+
+
+
 
